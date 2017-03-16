cd19260c366ff0b171704763a2ba11725152e1f4
liyunfu <859524441@qq.com>
2017-03-16 09:16:20 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
-
+    //改一下
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
